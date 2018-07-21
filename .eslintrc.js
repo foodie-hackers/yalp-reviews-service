c703e8283682ddb787bf652fd13f4bf15e828689
Chloe Lin <chloewjlin@gmail.com>
2018-07-20 19:07:59 -0700

    Configure ESLint with Airbnb JavaScript Style Guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,37 @@
-/**
- * These rules enforce the Hack Reactor Style Guide
- *
- * Visit this repo for more information:
- *   https://github.com/reactorcore/eslint-config-hackreactor
- */
-
 module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
+    "env": {
+        "browser": true,
+        "commonjs": true
+    },
+    "parser": "babel-eslint",
+    "extends": "airbnb-base",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import",
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
 };
