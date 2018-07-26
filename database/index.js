61bc046927b7f9f6acdc9bed3d990122df39f6d5
Chloe Lin <chloewjlin@gmail.com>
2018-07-26 00:35:37 -0700

    Complete query function that gets all reviews for one restaurant id
    This function passes the tests with node and can console.log all
    reviews associated with one unique restaurant id

--- database/index.js
+++ database/index.js
@@ -15,3 +15,20 @@ connection.connect(((err) => {
 
   console.log('connected as id ' + connection.threadId);
 }));
+
+// get all the reviews associated with one restaurant
+const getAllReviews = function(restaurantId, callback) {
+  const query = `SELECT * FROM reviews where business_id = ${restaurantId};`;
+  connection.query(query, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, data);
+      console.log(`Successfully query all the reviews associated with restaurant id ${restaurantId}.`);
+    }
+  });
+}
+
+module.exports = {
+  getAllReviews
+};
