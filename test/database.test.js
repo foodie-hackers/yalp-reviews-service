0fcd864104fc36560e195639db11b58896bb7368
Chloe Lin <chloewjlin@gmail.com>
2018-07-27 11:47:34 -0700

    Revise tests for database connection using done for async issue
    To solve async issue, end connection after the test by adding
    this line of code:
    
    afterAll(() => db.connection.end());
    
    Add three tests to test the database.

--- test/database.test.js
+++ test/database.test.js
@@ -1,43 +1,93 @@
-var db = require('../database/index');
+const db = require('../database/index.js');
 
-test('Should get expected reviews for "The Bird" when the given restaurant id is 6', () => {
-  var reviewsForRestaurant =  db.getAllReviews(6, (err, data) => {
+afterAll(() => db.connection.end());
+
+test('Should get expected reviews for "The Bird" when the given restaurant id is 6', (done) => {
+  function callback(error, data) {
+    expect(data[0].text).toBe("\"One of the best chicken sandwiches I\'ve ever had. Quick line and fast service makes what looks like a long wait short. If you\'re in the area it\'s must try\"");
+    done();
+  }
+
+  db.getAllReviews(6, (err, data) => {
     if (err) {
       throw err;
     } else {
-      return data;
+      callback(null, data);
     }
   });
-  
-  setTimeout(function() {
-    expect(reviewsForRestaurant[0].text).to.be.equal("One of the best chicken sandwiches I\'ve ever had. Quick line and fast service makes what looks like a long wait short. If you\'re in the area it\'s must try");
-  }, 1000);
 });
 
-test('Should get two reviews for "Ijji Sushi" with restaurant id 83', () => {
-  var reviewsForRestaurant = db.getAllReviews(83, (err, data) => {
+test('Should get two reviews for "Ijji Sushi" with restaurant id 83', (done) => {
+  function callback(error, data) {
+    expect(data.length).toBe(2);
+    done();
+  }
+
+  db.getAllReviews(83, (err, data) => {
     if (err) {
       throw err;
     } else {
-      return data.length;
+      callback(null, data);
     }
-  });
-  
-  setTimeout(function() {
-    expect(reviewsForRestaurant).to.be.equal(2);
-  }, 1000);
+  }); 
 });
 
-test('Should get reviews for "Eight Tables by George Chen" with restaurant id 99', function() {
-  var reviewsForRestaurant = db.getAllReviews(99, (err, data) => {
+test('Should get reviews for "Eight Tables by George Chen" with restaurant id 99', function(done) {
+  function callback(error, data) {
+    expect(data[0].name).toBe("\"Eight Tables by George Chen\"");
+    done();
+  }
+
+  db.getAllReviews(99, (err, data) => {
     if (err) {
       throw err;
     } else {
-      return data;
+      callback(null, data);
     }
   });
-  
-  setTimeout(function() {
-    expect(reviewsForRestaurant[0].name).to.be.equal("Eight Tables by George Chen");
-  }, 1000);
+});
+
+test('should have 234 restaurant reviews in database', (done) => {
+  function callback(error, data) {
+    expect(data.length).toBe(234);
+    done();
+  }
+
+  db.connection.query('SELECT * FROM reviews', (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      callback(null, results);
+    }
+  });
+});
+
+test('should have 100 restaurants in database', (done) => {
+  function callback(error, data) {
+    expect(data.length).toBe(100);
+    done();
+  }
+
+  db.connection.query('SELECT * FROM business', (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      callback(null, results);
+    }
+  });
+});
+
+test('should have 234 users in database', (done) => {
+  function callback(error, data) {
+    expect(data.length).toBe(234);
+    done();
+  }
+
+  db.connection.query('SELECT * FROM users', (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      callback(null, results);
+    }
+  });
 });
