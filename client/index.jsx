486b1fe34e7dbb96c6e8667d985b1fc905e7f1cc
Chloe Lin <chloewjlin@gmail.com>
2018-07-27 01:21:08 -0700

    Use React router to get URL parameter
    The server now serves up all the reviews associated with one
    restaurant when receiving a GET request to `http://localhost
    :3003/*` (* is a restaurant id).

--- client/index.jsx
+++ client/index.jsx
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, withRouter } from 'react-router-dom';
 import $ from 'jquery';
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
+    
+    this.state = {
+      id: this.props.location.pathname.substr(1),
+      reviews: []
+    }
+  }
+
+  componentDidMount() {
+    this.getAllReviews(this.state.id);
+  }
+
+  getAllReviews(restaurantId) {
+    $.ajax({
+      url: 'http://localhost:3003/restaurants/' + restaurantId + '/reviews',
+      type: 'GET',
+      dataType: 'json',
+      success: (data) => {
+        console.log('Ajax GET works', data);
+        this.setState({
+          reviews: data
+        });
+      },
+      error: (err) => {
+        console.log('Ajax GET fails', err);
+      }
+    });
   }
 
   render() {
     return (
-      <div>Yalp Reviews</div>
+      <div className="review-list">
+        <ul>
+          {this.state.reviews.map((review, i) => <div key={i}>
+            <div>
+              {review.name} - {review.user}
+            </div>
+            <div>
+              {review.text}
+            </div>
+          </div>)}
+        </ul>
+      </div>
     );
   }
 }
+const AppWithRouter = withRouter(props => <App {...props} />);
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<BrowserRouter>
+  <AppWithRouter />
+  </BrowserRouter>, document.getElementById('app'));
