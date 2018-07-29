fd1712ac349a180565fa0413f8869d304bd7871e
Chloe Lin <chloewjlin@gmail.com>
2018-07-29 16:24:22 -0700

    Resolving conflicts between Master and Component

--- public/bundle.js
+++ public/bundle.js
@@ -63,12 +63,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 84);
+/******/ 	return __webpack_require__(__webpack_require__.s = 33);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 84:
+/***/ 33:
 /* no static exports found */
 /* all exports used */
 /*!**************************!*\
@@ -76,7 +76,7 @@
   \**************************/
 /***/ (function(module, exports) {
 
-eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or directory, open '/Users/Chloeweijoulin/Desktop/yalp-reviews-service/client/index.jsx'\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiODQuanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiIiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///84\n");
+eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or directory, open '/Users/Chloeweijoulin/Desktop/yalp-reviews-service/client/index.jsx'\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMzMuanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiIiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///33\n");
 
 /***/ })
 
