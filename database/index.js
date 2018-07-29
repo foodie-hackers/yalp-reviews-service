e65adc3e4e48d1c1100aa7eedb84cc42ebfb81bc
Chloe Lin <chloewjlin@gmail.com>
2018-07-28 17:00:36 -0700

    Revise query to get user location, friends, and photos

--- database/index.js
+++ database/index.js
@@ -18,7 +18,7 @@ connection.connect(((err) => {
 
 // get all the reviews and user data associated with one restaurant
 const getAllReviews = function(restaurantId, callback) {
-  const query = `SELECT reviews.name, reviews.date, reviews.text, reviews.stars, reviews.useful, reviews.funny, reviews.cool, users.name as user, users.review_count, users.avatar FROM users, reviews, business WHERE users.id = reviews.user_id and business.id = reviews.business_id and reviews.business_id = ${restaurantId};`;
+  const query = `SELECT reviews.name, reviews.date, reviews.text, reviews.stars, reviews.useful, reviews.funny, reviews.cool, users.name as user, users.review_count, users.avatar, users.city, users.state, users.friends, users.photos FROM users, reviews, business WHERE users.id = reviews.user_id and business.id = reviews.business_id and reviews.business_id = ${restaurantId};`;
   connection.query(query, (err, data) => {
     if (err) {
       callback(err);
