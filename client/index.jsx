25b8a1a8492464457393bd9eb7464c0d1cb6cb39
Chloe Lin <chloewjlin@gmail.com>
2018-07-30 12:31:44 -0700

    Render restaurant name dynamically in header

--- client/index.jsx
+++ client/index.jsx
@@ -63,13 +63,36 @@ const Review = styled.p `
   color: #38393A;
   font-size: 17px;
 `;
+const ReviewButtons = styled.button `
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-right: 14px;
+  padding-left: 12px;
+  margin: 5px;
+  font-size: 15px;
+  color: #5D5C5B;
+  font-weight: bold;
+  border-radius: 5px;
+  cursor: pointer;
+`
 const rating = stars => ({
-  backgroundColor: stars > 3 ? "red" : stars > 0 ? "orange" : "gray",
+  backgroundColor: stars > 3 ? "#DA2110" : stars > 0 ? "orange" : "gray",
   color: "white",
   margin: "1px",
   padding: "2px",
   borderRadius: "2px"
 });
+const Header = styled.div `
+  color: #DA2110;
+  font-family: arial;
+  font-size: 25px;
+  padding: 10px;
+`;
+const RestaurantName = styled.span `
+  color: #4A4444;
+  font-family: arial;
+  font-size: 23px;
+`;
 
 class App extends Component {
   constructor(props) {
@@ -105,9 +128,14 @@ class App extends Component {
   render() {
     return (
       <div className="review-list">
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous"></link>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossOrigin="anonymous"></link>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         <ul>
+          <Header><strong>Recommended Reviews</strong> {this.state.reviews.map((review, i) => {
+            if (i === 0) { 
+              return <RestaurantName key={i}> for {review.name}</RestaurantName>;
+            }
+          })}</Header>
           {this.state.reviews.map((review, i) => 
           <Div key={i}>
             <ReviewSideBar>
@@ -124,7 +152,7 @@ class App extends Component {
                   <strong> {review.review_count} </strong> reviews 
                 </ReviewCount>
                 <PhotoCount> 
-                  <i class="fas fa-camera" style={{color: "#ED6E10"}}></i>
+                  <i className="fas fa-camera" style={{color: "#ED6E10"}}></i>
                   <strong> {review.photos} </strong> photos 
                 </PhotoCount>
               </UserInfo>
@@ -133,6 +161,10 @@ class App extends Component {
             {Array(5).fill().map((e, i) => <span key={i} className="fa fa-star" style={rating(i < review.stars ? review.stars : 0)} />)}
               <ReviewDate> {review.date.replace(/-/g, '/')} </ReviewDate>
               <Review> {review.text} </Review>
+              <ReviewButtons><i className="fas fa-lightbulb" style={{padding: "0.3em"}}></i>Useful</ReviewButtons>
+              <ReviewButtons><i className="far fa-grin" style={{padding: "0.3em"}}></i>Funny</ReviewButtons>
+              <ReviewButtons><i className="far fa-grin-hearts" style={{padding: "0.3em"}}></i>Cool</ReviewButtons>
+              {/* <ReviewButtons><i className="fas fa-flag" style={{padding: "4px"}}></i></ReviewButtons> */}
             </ReviewWrapper>
           </Div>)}
         </ul>
