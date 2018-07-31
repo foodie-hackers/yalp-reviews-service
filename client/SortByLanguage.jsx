ae7f4693c8c0dfa13b6fe4fa2e978ed3a02ebaf6
Chloe Lin <chloewjlin@gmail.com>
2018-07-31 11:00:44 -0700

    Add SortByLanguage dropdown

--- client/SortByLanguage.jsx
+++ client/SortByLanguage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const SortByLanguageDropDown  = styled.div `
+const SortByLanguageDropDown = styled.div `
   border: "white";
   padding: 0.9em;
   margin: 2px;
@@ -11,10 +11,23 @@ const SortByLanguageDropDown  = styled.div `
   color: #5D5C5B;;
   cursor: pointer;
 `;
+const Dropdown = styled.span `
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  padding: 12px 16px;
+  z-index: 1;
+`;
 
 const SortByLanguage = (props) => {
   return (
-    <SortByLanguageDropDown>Language <b>English ({props.reviewCount}) </b><i className="fas fa-caret-down"></i></SortByLanguageDropDown>
+    <div>
+    <SortByLanguageDropDown>
+      Language <b>English ({props.reviewCount}) </b><i className="fas fa-caret-down"></i></SortByLanguageDropDown>
+    <Dropdown>Yalp Sort</Dropdown>
+    </div>
   );
 }
 
