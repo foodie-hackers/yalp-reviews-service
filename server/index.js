2f0be45d195fe3792420e62bc23cc1786af6ec2f
Chloe Lin <chloewjlin@gmail.com>
2018-08-03 20:07:23 -0700

    Use environment variable for Elastic Beanstalk

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const db = require('../database/index.js');
 const path = require('path');
 const app = express();
-const PORT = 3003;
+// const PORT = 3003;
+app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -33,7 +34,7 @@ app.use('/*', (req, res) => {
 });
 
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
+  app.listen(app.get('port'), () => {
     console.log('Example app listening on port 3003');
   });
 }
